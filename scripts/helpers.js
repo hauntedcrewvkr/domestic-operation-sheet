7a794641273dae6370356b3ade2aa7770f9ce87e
hautedcrewvkr <www.vikram.roy.372@gmail.com>
2025-06-26 07:47:14 +0530

    done

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -210,7 +210,7 @@ function setViewActions(element) {
     gsheet.domesticOperationSheet.actionAccess.header ??= data.header;
 
     for (row of data.data) {
-      if (row.access.value.contains(app.user.props.email) && row.type.value == `View Action`) {
+      if (row.access.value.includes(app.user.props.email) && row.type.value == `View Action`) {
         element.append(schema2el({ tag: `li`, attr: { title: row.action.value }, func: [getIcon] }));
       }
     }
