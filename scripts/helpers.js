b2714727d67b2293816fb503526018757864e0ed
hautedcrewvkr <www.vikram.roy.372@gmail.com>
2025-06-27 06:25:27 +0530

    done

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -563,8 +563,8 @@ function setTableHeaders(element) {
 }
 
 function setTableRows(element, props = { page: 1, view: `Orders`, rpp: 50 }) {
-  console.log(gsheet.domesticOperationSheet);
-  const data = gsheet.domesticOperationSheet[props.view].data;
+  console.log(gsheet.domesticOperationSheet[props.view]);
+  console.log(gsheet.domesticOperationSheet[props.view]);
   const data = gsheet.domesticOperationSheet[props.view].data;
   let start = props.page > 0 ? props.rpp * (props.page - 1) : 0;
   const end = Math.min(start + props.rpp, data.length);
