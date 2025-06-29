2233b2fe9530fb43f5bb12a8c827ccd0bafbc647
hautedcrewvkr <www.vikram.roy.372@gmail.com>
2025-06-30 05:03:32 +0530

    done

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -21,7 +21,7 @@ function actionRouter(e) {}
 //---------------------------------------------<( set-action-helper-function )>-
 function setAction(element) {
   const actionName = element.getAttribute('actionname');
-
+  console.log(app.cta);
   element.append(actionName == 'Sub Action' ? app.cta[actionName].cloneNode(true) : app.cta[actionName]);
 }
 
