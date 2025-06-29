270deeb68b0f66c7705713ac5ca444a8ed702230
hautedcrewvkr <www.vikram.roy.372@gmail.com>
2025-06-30 04:15:58 +0530

    done

--- scripts/async.js
+++ scripts/async.js
@@ -244,7 +244,7 @@ async function setDropdowns(element) {
     return (a.column_name?.value || '').toLowerCase().localeCompare((b.column_name?.value || '').toLowerCase());
   }
   function pocSort(a, b) {
-    console.log([a, b]);
-    return a.POC.value.toLocaleCompare(b.POC.value);
+    console.log([a.POC.value.toString(), b.POC.value.toString()]);
+    return a.POC.value.toString().toLocaleCompare(b.POC.value.toString());
   }
 }
