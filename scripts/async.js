895f5e1e8f4529a22a197d2bf4d0e5a26d526b7d
hautedcrewvkr <www.vikram.roy.372@gmail.com>
2025-07-02 10:07:57 +0530

    checking

--- scripts/async.js
+++ scripts/async.js
@@ -90,7 +90,7 @@ async function setFilterViews(data) {
       if (!gsheet.domesticOperationSheet[view]) gsheet.domesticOperationSheet[view] = {};
       if (!gsheet.domesticOperationSheet[view].data) gsheet.domesticOperationSheet[view].data = [];
 
-      console.log([json, gsheet.filters[view], filterCheck({ json: json, filter: gsheet.filters[view] }), view]);
+      if (json.ID.value == 'ORDR-1748112676') console.log([json, gsheet.filters[view], filterCheck({ json: json, filter: gsheet.filters[view] }), view]);
       if (filterCheck({ json: json, filter: gsheet.filters[view] })) {
         gsheet.domesticOperationSheet[view].data.push(json);
       }
