4b7e9175b69cd081567d0b7d096e73662134b370
hautedcrewvkr <www.vikram.roy.372@gmail.com>
2025-07-03 01:58:21 +0530

    animation fixed

--- scripts/async.js
+++ scripts/async.js
@@ -54,7 +54,7 @@ async function start() {
   }
 
   try {
-    // setTimeout(() => removeLoader(), 3000);
+    setTimeout(() => removeLoader(), 3000);
   } catch (error) {
     console.log(`Error Occurred: ${error}`);
   }
