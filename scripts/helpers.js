d2b6c8f0fddcf057f01d826b2c2b728415498693
hautedcrewvkr <www.vikram.roy.372@gmail.com>
2025-07-03 06:23:51 +0530

    done

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -130,21 +130,21 @@ function removeElement({ element, urgent = false }) {
 
 //--------------------------------------<( set-table-headers-helper-function()>-
 function setTableHeaders(element) {
-  element.append(
-    schema2el({
-      tag: 'th',
-      sub: [
-        {
-          tag: 'div',
-          attr: { class: 'th-holder th-search-holder' },
-          sub: [
-            { tag: 'span', attr: { class: 'icon-span' }, sub: [{ tag: 'i', attr: { class: 'ph ph-magnifying-glass' } }] },
-            { tag: 'input', attr: { type: 'text' } },
-          ],
-        },
-      ],
-    })
-  );
+  // element.append(
+  //   schema2el({
+  //     tag: 'th',
+  //     sub: [
+  //       {
+  //         tag: 'div',
+  //         attr: { class: 'th-holder th-search-holder' },
+  //         sub: [
+  //           { tag: 'span', attr: { class: 'icon-span' }, sub: [{ tag: 'i', attr: { class: 'ph ph-magnifying-glass' } }] },
+  //           { tag: 'input', attr: { type: 'text' } },
+  //         ],
+  //       },
+  //     ],
+  //   })
+  // );
 
   for (const [group, columns] of Object.entries(gsheet.columnGroup)) {
     if (Array.isArray(columns) && columns.length > 0) {
@@ -193,7 +193,7 @@ function setTableRows(page = 1) {
 
   while (start < end) {
     const tr = schema2el({ tag: 'tr', attr: { class: 'table-row', onclick: 'seeDetails(event)', rowNum: start + 2 } });
-    tr.append(schema2el({ tag: 'td', attr: { class: 'sub-action-td' }, sub: [{ tag: 'div', attr: { class: 'tr-actions', actionname: 'Sub Action' }, sub: [{ tag: 'span', attr: { class: 'icon-span' }, sub: [{ tag: 'i', attr: { class: 'ph ph-corners-out' } }] }] }] }));
+    // tr.append(schema2el({ tag: 'td', attr: { class: 'sub-action-td' }, sub: [{ tag: 'div', attr: { class: 'tr-actions', actionname: 'Sub Action' }, sub: [{ tag: 'span', attr: { class: 'icon-span' }, sub: [{ tag: 'i', attr: { class: 'ph ph-corners-out' } }] }] }] }));
 
     for (const [group, columns] of Object.entries(gsheet.columnGroup)) {
       const td = schema2el({ tag: 'td', sub: [{ tag: 'div', attr: { headname: group } }] });
