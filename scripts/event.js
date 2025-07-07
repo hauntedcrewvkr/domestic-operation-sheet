181c0eab530e09d9164c0f743a464c817f014772
hautedcrewvkr <www.vikram.roy.372@gmail.com>
2025-07-08 04:37:38 +0530

    done

--- scripts/event.js
+++ scripts/event.js
@@ -155,13 +155,15 @@ function seeDetails(e) {
   const td = fx.$$('td div[headname]', row);
   const extraViews = fx.$('main .extra-views');
 
+  const rownum = td.getAttribute('rownum');
+
   const mainSchema = {
     tag: 'div',
     attr: { class: 'detail-view' },
     sub: [
       { tag: 'legend', text: 'DETAILS', attr: { class: 'details-heading' } },
       { tag: 'button', attr: { class: 'detail-close-btn', onclick: 'removeDetailView(event)' } },
-      { tag: 'div', attr: { class: 'sub-action-div' } },
+      { tag: 'div', attr: { class: 'sub-action-div', rownum: rownum } },
     ],
   };
 
